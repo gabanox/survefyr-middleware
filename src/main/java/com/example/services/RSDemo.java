73e58077f9fd914b50f623b50ce6f4615cbedcaf
Pedro Cazu <training@pcabrera.com>
2014-06-20 13:09:07 -0500

    RSDemo

---
+++ src/main/java/com/example/services/RSDemo.java
@@ -0,0 +1,20 @@
+package com.example.services;
+
+//import com.example.models.Time;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/rsdemo")
+@Produces(MediaType.APPLICATION_JSON)
+public class RSDemo {
+
+    @GET
+    public String get() {
+        return "RSDemo";
+    }
+
+}
+
