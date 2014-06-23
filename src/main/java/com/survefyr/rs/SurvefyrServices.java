f9095c380e7f671fe339d26f87e9accb30517ccd
Pedro Cazu <training@pcabrera.com>
2014-06-23 17:21:37 -0500

    Data on Surveys

--- src/main/java/com/survefyr/rs/SurvefyrServices.java
+++ src/main/java/com/survefyr/rs/SurvefyrServices.java
@@ -19,10 +19,24 @@ public class SurvefyrServices {
 	final private String helloJSON = "{'name'='Survefyr','message'='Hello, lets Surveyyyy :)'}";
     @GET
     public List<Survey> get() {
-    	List<Survey> s = new ArrayList<Survey>();
-    	s.add(new Survey());
-    	s.add(new Survey());
-        return s;
+    	List<Survey> surveys = new ArrayList<Survey>();
+    	Survey s = new Survey();
+    	s.setAplicant("Aplicant 1");
+    	s.setAplicator("Aplicator 1");
+    	s.setCreator("Creator 1");
+    	s.setOwner("Owner 1");
+    	
+
+    	
+    	Survey s2 = new Survey();
+    	s2.setAplicant("Aplicant 2");
+    	s2.setAplicator("Aplicator 2");
+    	s2.setCreator("Creator 2");
+    	s2.setOwner("Owner 2");
+    	
+    	    	surveys.add(s);
+    	    	surveys.add(s2);
+        return surveys;
     }
 
 }
