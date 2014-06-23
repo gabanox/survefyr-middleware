864da738cb68d189c1333a49a6e07763790ad716
Pedro Cazu <training@pcabrera.com>
2014-06-23 17:29:34 -0500

    Test data on /surveys

--- src/main/java/com/survefyr/rs/SurvefyrServices.java
+++ src/main/java/com/survefyr/rs/SurvefyrServices.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.survefyr.model.data.Question;
 import com.survefyr.model.data.QuestionItem;
 import com.survefyr.model.data.Survey;
 
@@ -25,14 +26,68 @@ public class SurvefyrServices {
     	s.setAplicator("Aplicator 1");
     	s.setCreator("Creator 1");
     	s.setOwner("Owner 1");
-    	
-
+    	List<Question> questions = new ArrayList<Question>();
+    	Question q1 = new Question();
+    	q1.setQuestionDescription("Question Description 1");
+    	List<QuestionItem> questionItems = new ArrayList<QuestionItem>();
+    	QuestionItem qi = new QuestionItem();
+    	qi.setDescription("description 1");
+    	qi.setValue("value 1");
+     	QuestionItem qi2 = new QuestionItem();
+    	qi.setDescription("description 2");
+    	qi.setValue("value 2");
+    	questionItems.add(qi);
+    	questionItems.add(qi2);
+    	q1.setQuestionItems(questionItems);
+    	Question q2 = new Question();
+    	q1.setQuestionDescription("Question Description 2");
+    	List<QuestionItem> questionItems2 = new ArrayList<QuestionItem>();
+    	QuestionItem qi21 = new QuestionItem();
+    	qi.setDescription("description 1");
+    	qi.setValue("value 1");
+     	QuestionItem qi22 = new QuestionItem();
+    	qi.setDescription("description 2");
+    	qi.setValue("value 2");
+    	questionItems.add(qi21);
+    	questionItems.add(qi22);
+    	q1.setQuestionItems(questionItems2);
+    	questions.add(q1);
+    	questions.add(q2);
     	
     	Survey s2 = new Survey();
     	s2.setAplicant("Aplicant 2");
     	s2.setAplicator("Aplicator 2");
     	s2.setCreator("Creator 2");
     	s2.setOwner("Owner 2");
+       	List<Question> questionsS2 = new ArrayList<Question>();
+    	Question q1S2 = new Question();
+    	q1.setQuestionDescription("Question Description 1");
+    	List<QuestionItem> questionItemsS2 = new ArrayList<QuestionItem>();
+    	QuestionItem qiS2 = new QuestionItem();
+    	qi.setDescription("description 1");
+    	qi.setValue("value 1");
+     	QuestionItem qi2S2 = new QuestionItem();
+    	qi.setDescription("description 2");
+    	qi.setValue("value 2");
+    	questionItems.add(qiS2);
+    	questionItems.add(qi2S2);
+    	q1.setQuestionItems(questionItems);
+    	Question q2S2 = new Question();
+    	q1.setQuestionDescription("Question Description 2");
+    	List<QuestionItem> questionItems2S2 = new ArrayList<QuestionItem>();
+    	QuestionItem qi21S2 = new QuestionItem();
+    	qi.setDescription("description 1");
+    	qi.setValue("value 1");
+     	QuestionItem qi22S2 = new QuestionItem();
+    	qi.setDescription("description 2");
+    	qi.setValue("value 2");
+    	questionItems.add(qi21S2);
+    	questionItems.add(qi22S2);
+    	q1.setQuestionItems(questionItems2S2);
+    	questions.add(q1S2);
+    	questions.add(q2S2);
+    	
+    	
     	
     	    	surveys.add(s);
     	    	surveys.add(s2);
